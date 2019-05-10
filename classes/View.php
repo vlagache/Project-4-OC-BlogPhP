8aa3ec13ea8008749e3199972f4e61dd95fc33e1
vlagache <v1.lagache@gmail.com>
2019-05-10 11:06:07 +0200

    Script JS Url & header(Location:)

--- classes/View.php
+++ classes/View.php
@@ -20,11 +20,14 @@ class View
      require(VIEW.'template.php');
 
    }
+   /**
+    * [header header('Location:') does not work on 1&1 , use of a JS script]
+    * @return [String] [description]
+    */
    public function header()
    {
      $template = $this->template;
-     header('Location:' . HOST . $template);
-
-
+     // header('Location:' . HOST . $template);
+     echo '<script>window.location.href=""+"'.HOST.$template.'"+"";</script>';
    }
 }
