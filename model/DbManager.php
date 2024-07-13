b07769c05a92115f3277e1dbdcdd7807339bc612
vlagache <v1.lagache@gmail.com>
2024-07-13 11:20:50 +0200

    fix : hidden connection info

--- model/DbManager.php
+++ model/DbManager.php
@@ -5,8 +5,15 @@
 
     public function __construct()
     {
-      $db = new PDO('mysql:host=localhost;dbname=p4-oc;charset=utf8', 'root', 'root');
+      $configDb = parse_ini_file("config.ini");
+      $loginDb = $configDb['dblogin'];
+      $passwordDb = $configDb['dbpassword'];
+
+
+      $db = new PDO('mysql:host=db5000071771.hosting-data.io;dbname=dbs66372;charset=utf8', $loginDb, $passwordDb);
+      // $db = new PDO('mysql:host=localhost;dbname=p4-oc;charset=utf8', 'root', 'root');
       $this->db = $db;
+
     }
 
     protected function dbConnect()
